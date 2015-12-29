0ba08a4247b6f12bd0e16a20921d816c32fbfa9e
Rapscaliwag <dusty.hooligan@gmail.com>
2015-12-29 13:00:27 -0700

    The start game button works!

--- Assets/Scripts/StartGame.cs
+++ Assets/Scripts/StartGame.cs
@@ -1,15 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class StartGame : MonoBehaviour {
+public class StartGame : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public void StartTheGame (string changeSceneTo)
+    {
+        Application.LoadLevel(changeSceneTo);
+    }
 }
