bfe7ad15795678bb36a2f416549b03afbbfc3e33
Rapscaliwag <dusty.hooligan@gmail.com>
2015-12-30 13:05:12 -0700

    Created health bar!

---
+++ Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControls : MonoBehaviour
+{
+    int damageAmount = 10;
+
+	void Update ()
+    {
+        //This will allow us to test taking health away from player
+        if (Input.anyKeyDown)
+            this.transform.GetComponent<PlayerHealth>().TakeDamage(damageAmount);
+	}
+}
---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int initialHealth = 100;
+    public int currentHealth;
+    public Slider healthBar;
+
+    void Awake ()
+    {
+        currentHealth = initialHealth;
+    }
+
+    public void TakeDamage (int damageTaken)
+    {
+        currentHealth -= damageTaken;
+        healthBar.value = currentHealth;
+    }
+}
