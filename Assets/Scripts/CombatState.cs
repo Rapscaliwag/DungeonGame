aa93c6d31d6dedd09b71f947a74bda5d632e294e
Rapscaliwag <dusty.hooligan@gmail.com>
2015-12-30 17:07:29 -0700

    Combat states!

---
+++ Assets/Scripts/CombatState.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CombatState : MonoBehaviour
+{
+    public enum CombatStates
+    {
+        START,
+        PLAYERMOVE,
+        ENEMYMOVE,
+        LOSE,
+        WIN
+    }
+
+    private CombatStates currentState;
+
+	void Start ()
+    {
+        currentState = CombatStates.START;
+	}
+	
+	void Update ()
+    {
+	    switch(currentState)
+        {
+            case (CombatStates.START):
+                break;
+            case (CombatStates.PLAYERMOVE):
+                break;
+            case (CombatStates.ENEMYMOVE):
+                break;
+            case (CombatStates.LOSE):
+                break;
+            case (CombatStates.WIN):
+                break;
+        }
+	}
+}
